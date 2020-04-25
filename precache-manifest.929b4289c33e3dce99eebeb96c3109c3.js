b10d9970d436b8f1595063ed0ec3835e0dc2dce2
Garett Tok Ern Liang <36098015+walnutdust@users.noreply.github.com>
2020-04-26 01:13:42 +0800

    Updates

---
+++ precache-manifest.929b4289c33e3dce99eebeb96c3109c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac634e2db0b9f9160ddf5e7394b8787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fdb456d19be3dd76025",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52ec7d6d272e94b03598",
+    "url": "/static/js/2.01639fb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.01639fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fdb456d19be3dd76025",
+    "url": "/static/js/main.5914614d.chunk.js"
+  },
+  {
+    "revision": "ded68e03c3d829488e78",
+    "url": "/static/js/runtime-main.e846b642.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
