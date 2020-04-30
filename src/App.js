f4c3d6f59105134009a1e98780fdad965b7f15da
Garett Tok Ern Liang <36098015+walnutdust@users.noreply.github.com>
2020-05-01 00:40:04 +0800

    some changes

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ import '@elastic/eui/dist/eui_theme_light.css';
 import './App.css';
 
 const App = () => {
-  const sections = ['About Me', 'Experience', 'Projects', 'Extracurriculars','Contact Me'];
+  const sections = ['About Me', 'Experience', 'Projects', 'Extracurriculars', 'Contact Me'];
 
   const toHref = (title) => {
     return '#' + title.toLowerCase().split(' ').join('-');
@@ -47,10 +47,11 @@ const App = () => {
       <EuiPageHeader>
         <EuiPageHeaderSection>
           <EuiTitle size='l'>
-            <h1>Kaizen Conroy &emsp;
-            <EuiIcon type={williamsSvg} size="xxl"/> &emsp;
-            <EuiIcon type="logoAWS" size="xxl" /> &emsp;
-            <EuiIcon type={juniSvg} size="xxl" /> &emsp;
+            <h1>
+              Kaizen Conroy &emsp;
+              <EuiIcon type={williamsSvg} size='xxl' /> &emsp;
+              <EuiIcon type='logoAWS' size='xxl' /> &emsp;
+              <EuiIcon type={juniSvg} size='xxl' /> &emsp;
             </h1>
           </EuiTitle>
         </EuiPageHeaderSection>
@@ -72,46 +73,51 @@ const App = () => {
           <PageSection title='About Me'>
             <EuiText>
               <p>
-                <EuiIcon type={williamsSvg} size="l" />
-                // why is this not a comment lol <EuiImage size="l" alt='some text' url={'Williams1.png'} />
-
-                I am a junior at <b>Williams College</b> pursuing a bachelor of arts in Computer Science.
+                {/* jsx comments should be written like this instead */}
+                // why is this not a comment lol
+                <EuiImage size='l' alt='some text' url={'Williams1.png'} />I am a junior at{' '}
+                <EuiIcon
+                  type={williamsSvg}
+                  size='l'
+                  style={{ display: 'inline', margin: 0, width: 'auto' }}
+                />
+                <b>Williams College</b> pursuing a bachelor of arts in Computer Science.
               </p>
               <p>
-                <center><EuiIcon type="logoAWS" size="l" /></center>
+                <center>
+                  <EuiIcon type='logoAWS' size='l' />
+                </center>
               </p>
               <p>
-                I will be interning at <b>Amazon Web Services</b> this summer as part of the <b>Cloud Development Kit</b> team.
-                I am beyond excited for this opportunity and am looking to get my hands dirty in the form of a Fall Software Engineering Internship
+                I will be interning at <b>Amazon Web Services</b> this summer as part of the{' '}
+                <b>Cloud Development Kit</b> team. I am beyond excited for this opportunity and am
+                looking to get my hands dirty in the form of a Fall Software Engineering Internship
                 as well.
               </p>
               <p>
                 <center>
-                <EuiIcon type={juniSvg} size="original" />
-                //<EuiImage size="l" alt='some text' url={'./juni1.png'} />
+                  <EuiIcon type={juniSvg} size='original' />
+                  //
+                  <EuiImage size='l' alt='some text' url={juniSvg} />
                 </center>
               </p>
               <p>
-                During the school year, I teach computer science online as a <b>Senior Instructor</b> at
-                <b>Juni Learning</b>.
-                I help my kids develop skills in <b>Python</b> and <b>Java</b>, and I develop our top students for the USA Computing Olympiad.
-                Outside of teaching, I moonlight as a Python curriculum developer and recruitment coordinator for Juni Learning.
-                Some of the projects I have created and taught are a <b>Tic-Tac-Toe AI</b> that never loses, as well as a simulation of
+                During the school year, I teach computer science online as a{' '}
+                <b>Senior Instructor</b> at
+                <b>Juni Learning</b>. I help my kids develop skills in <b>Python</b> and <b>Java</b>
+                , and I develop our top students for the USA Computing Olympiad. Outside of
+                teaching, I moonlight as a Python curriculum developer and recruitment coordinator
+                for Juni Learning. Some of the projects I have created and taught are a{' '}
+                <b>Tic-Tac-Toe AI</b> that never loses, as well as a simulation of
                 <b> Conways Game of Life</b>.
               </p>
-              <p>
-                Lets connect! Feel free to contact me at zen.conroy@gmail.com.
-
-              </p>
+              <p>Lets connect! Feel free to contact me at zen.conroy@gmail.com.</p>
             </EuiText>
           </PageSection>
           <EuiSpacer />
 
-
-          <PageSection title='Experience'>
-          </PageSection>
+          <PageSection title='Experience'></PageSection>
           <EuiSpacer />
-
 
           <PageSection title='Projects'>
             <EuiText>
@@ -154,8 +160,7 @@ const App = () => {
           </PageSection>
           <EuiSpacer />
 
-          <PageSection title='Extracurriculars'>
-          </PageSection>
+          <PageSection title='Extracurriculars'></PageSection>
           <EuiSpacer />
 
           <PageSection title='Contact Me'>
