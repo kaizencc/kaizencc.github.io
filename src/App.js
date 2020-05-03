d46a3e9ebc9658218b7e61ad3a73a239b61c72e3
Kaizen Conroy <Kaizen Conroy>
2020-05-02 22:21:37 -0400

    added most of the info

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ import {
   EuiPageHeaderSection,
   EuiPageSideBar,
   EuiSideNav,
+  EuiHeader,
+  EuiHeaderSectionItem,
+  EuiHeaderLogo,
+  EuiHeaderLink,
+  EuiHeaderLinks,
   EuiSpacer,
 } from '@elastic/eui';
 import Header from './Header';
@@ -26,6 +31,15 @@ import juniSvg from './juni1.png';
 import williams from './Williams1.png';
 import kaizen from './kaizen_linkedin.jpg';
 import columbia from './columbia.jpg';
+import sas from './sas_logo2.jpg';
+import linkedin from './linkedin.png';
+import wufo from './wufo.jpg';
+import piano from './piano.jpg';
+import taiwan from './taiwan.png';
+
+// get our fontawesome imports
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // Other themes are also available - see files at
 // node_modules/@elastic/eui/dist/
@@ -64,7 +78,22 @@ const App = () => {
 
   return (
     <div className='App'>
-      <Header sections={sections} className='sticky' />
+    <EuiHeader>
+        <EuiHeaderSectionItem border="right">
+          <EuiHeaderLogo
+            iconType='logoGmail'
+            href="#"
+          />
+        </EuiHeaderSectionItem>
+
+        <EuiHeaderLinks>
+          <EuiHeaderLink href="#">
+          Resume
+          </EuiHeaderLink>
+        </EuiHeaderLinks>
+      </EuiHeader>
+      <Header sections={sections} className='sticky' 
+      />
       <EuiPage restrictWidth={true}>
         <EuiPageSideBar>
         <EuiImage
@@ -82,37 +111,37 @@ const App = () => {
           <PageSection title='About Me'>
             <EuiText>
               <p>
-                I am a junior at{' '}
+                I am a junior at <b> Williams College</b> {' '}
                 <EuiIcon
                   type={williamsSvg}
                   size='l'
                   style={{ display: 'inline', margin: 0, width: 'auto' }}
                 />
-                <b> Williams College</b> pursuing a bachelor of arts in Computer Science.
+                 {' '} pursuing a bachelor of arts in Computer Science.
               </p>
 
               <p>
-                I will be interning at <EuiIcon type='logoAWS' size='l' /> <b> Amazon Web Services</b> this summer as part of the{' '}
+                I will be interning at <b> Amazon Web Services </b> <EuiIcon type='logoAWS' size='l' /> this summer as part of the{' '}
                 <b>Cloud Development Kit</b> team. I am beyond excited for this opportunity and am
                 looking to get my hands dirty in the form of a Fall Software Engineering Internship
                 as well.
               </p>
               <p>
                 I teach computer science online as a {' '}
-                <b>Senior Instructor</b> at {' '}
+                <b>Senior Instructor</b> at <b>Juni Learning</b>{' '}
                 <EuiIcon
                   type={juniSvg}
                   size='l'
                   style={{ display: 'inline', margin: 0, width: 'auto' }}
                 />
-                {' '}<b>Juni Learning</b>. I help my kids develop skills in <b>Python</b> and <b>Java</b>
+                {' '}. I help my kids develop skills in <b>Python</b> and <b>Java</b>
                 , and I develop our top students for the USA Computing Olympiad. Outside of
                 teaching, I moonlight as a Python curriculum developer and recruitment coordinator
                 for Juni Learning. Some of the projects I have created and taught are a{' '}
                 <b>Tic-Tac-Toe AI</b> that never loses, as well as a simulation of
                 <b> Conways Game of Life</b>.
               </p>
-              <p>Lets connect! Feel free to contact me at <EuiIcon type='logoGmail' size='l' /> zen.conroy@gmail.com.</p>
+              <p>Lets connect! Feel free to contact me at zen.conroy@gmail.com.</p>
             </EuiText>
           </PageSection>
           <EuiSpacer />
@@ -176,36 +205,38 @@ const App = () => {
             <EuiFlexGroup gutterSize='l'>
               <EuiFlexItem>
                 <EuiCard
-                  icon={<EuiIcon size='xxl' type='devToolsApp' />}
-                  title='Built Something'
-                  description='With like, a spanner and a screwdriver.'
+                  image = {williamsSvg}
+                  title='Ticket To Ride Board Builder'
+                  description='iOS, Swift, XCode'
                   footer={<EuiButton aria-label='Go to Developers Tools'>See it</EuiButton>}
                 />
               </EuiFlexItem>
               <EuiFlexItem>
                 <EuiCard
-                  icon={<EuiIcon size='xxl' type='devToolsApp' />}
-                  title='Built Something Else'
-                  description='With two hammers and a torch.'
+                  image = {williamsSvg}
+                  title='Microservice Application'
+                  description='Python, Flask, MySQL, AWS'
                   footer={<EuiButton aria-label='Go to Developers Tools'>See it</EuiButton>}
                 />
               </EuiFlexItem>
-              <EuiFlexItem>
-                <EuiCard
-                  icon={<EuiIcon size='xxl' type='devToolsApp' />}
-                  title='Built Another thing'
-                  description='How many things can I build?'
-                  footer={<EuiButton aria-label='Go to Developers Tools'>See it</EuiButton>}
-                />
-              </EuiFlexItem>
-              <EuiFlexItem>
-                <EuiCard
-                  icon={<EuiIcon size='xxl' type='devToolsApp' />}
-                  title='Built The THING'
-                  description='With a lot of gusto.'
-                  footer={<EuiButton aria-label='Go to Developers Tools'>See it</EuiButton>}
-                />
-              </EuiFlexItem>
+            </EuiFlexGroup>
+            <EuiFlexGroup gutterSize='l'>
+            <EuiFlexItem>
+              <EuiCard
+                image = {williamsSvg}
+                title='Heads-Up Poker AI'
+                description='Python'
+                footer={<EuiButton aria-label='Go to Developers Tools'>See it</EuiButton>}
+              />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiCard
+                image = {williamsSvg}
+                title='Juni Learning Curriculum'
+                description='Python'
+                footer={<EuiButton aria-label='Go to Developers Tools'>See it</EuiButton>}
+              />
+            </EuiFlexItem>
             </EuiFlexGroup>
           </PageSection>
           <EuiSpacer />
@@ -216,7 +247,7 @@ const App = () => {
             layout="horizontal"
             icon={<EuiIcon type={williamsSvg} size='xxl' />}
             title="Williams College"
-            description="B.A., Computer Science , September 2017 - May 2021">
+            description="B.A., Computer Science, September 2017 - May 2021">
             <EuiText size="s">
               <ul>
                 <li>Deans List, GPA: 3.52.</li>
@@ -233,7 +264,7 @@ const App = () => {
             layout="horizontal"
             icon={<EuiIcon type={columbia} size='xxl' />}
             title="Columbia University"
-            description="Visiting Student in Computer Science">
+            description="Visiting Student in Computer Science, September 2019 - December 2019">
             <EuiText size="s">
               <ul>
                 <li>GPA: 3.64. </li>
@@ -242,18 +273,98 @@ const App = () => {
             </EuiText>
             </EuiCard>
           </EuiFlexGroup>
+          <EuiSpacer />
+          <EuiSpacer />
+          <EuiFlexGroup gutterSize='l'>
+            <EuiCard
+            layout="horizontal"
+            icon={<EuiIcon type={sas} size='xxl' />}
+            title="Shanghai American School"
+            description="High School Diploma, September 2013 - May 2017">
+            <EuiText size="s">
+              <ul>
+                <li>Graduated <i> summa cum laude </i> with 3.92 unweighted GPA.</li>
+                <li>2-year Captain and MVP of Varsity Baseball Team.</li>
+              </ul>
+            </EuiText>
+            </EuiCard>
+          </EuiFlexGroup>
           </PageSection>
           <EuiSpacer />
 
           <PageSection title='Extracurriculars'>
+          <EuiText>
+            <p>At Williams:</p>
+          </EuiText>
+          <EuiFlexGroup gutterSize='l'>
+            <EuiFlexItem>
+              <EuiCard
+                icon={<EuiIcon size='xxl' type={williamsSvg} />}
+                title='Williams Students Online (WSO)'
+                description='Member of the Frontend Team developing skills in React and Javascript while working on school website. '
+              />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiCard
+                icon={<EuiIcon size='xxl' type={wufo} />}
+                title='Williams Ultimate Frisbee Organization (WUFO)'
+                description='Captain (2020), Logistics Manager (2019), 2nd team All-region (2019).'
+              />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+          <EuiSpacer />
+          <EuiText>
+            <p>Before Williams:</p>
+          </EuiText>
+          <EuiFlexGroup gutterSize='l'>
+            <EuiFlexItem>
+              <EuiCard
+                icon={<EuiIcon size='xxl' type={piano} />}
+                title='Classical Pianist'
+                description='Performed competitively from 2004-2017 in Virginia and Shanghai.'
+              />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiCard
+                icon={<EuiIcon size='xxl' type={sas} />}
+                title='SAS Varsity Baseball'
+                description='2-year Captain, 4-year starting shortstop, MVP.'
+              />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiCard
+                icon={<EuiIcon size='xxl' type={taiwan} />}
+                title='Volunteer Teacher'
+                description='Taught English to disadvantaged 5th graders in rural Taiwan.'
+              />
+            </EuiFlexItem>
+          </EuiFlexGroup>
           </PageSection>
           <EuiSpacer />
 
           <PageSection title='Contact Me'>
-            <EuiListGroup>
-              <EuiListGroupItem label='@kaizen3031593' iconType='logoGithub' />
-              <EuiListGroupItem label='zen.conroy@gmail.com' iconType='logoGmail' />
-            </EuiListGroup>
+          <EuiListGroup
+              listItems={[
+                {
+                  label: 'zen.conroy@gmail.com',
+                  href: '#',
+                  color: "text",
+                  iconType: 'logoGmail',
+                },
+                {
+                  label: '@kaizen3031593',
+                  href: 'https://github.com/kaizen3031593',
+                  color: "text",
+                  iconType: 'logoGithub',
+                },
+                {
+                  label: 'LinkedIn',
+                  href: 'https://linkedin.com/in/kaizenconroy',
+                  color: "text",
+                  iconType: 'logoGmail',
+                }]
+              }
+            />
           </PageSection>
         </EuiPageBody>
       </EuiPage>
