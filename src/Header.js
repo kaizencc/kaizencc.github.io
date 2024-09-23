86f3181837cf94f546b674786710ee8ce8b64a8b
Kaizen Conroy <kcc2@williams.edu>
2024-09-23 10:54:05 -0400

    update resume

--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,7 @@ import {
 } from '@elastic/eui';
 import PropTypes from 'prop-types';
 
-import resume from './kaizen_conroy_resume_2023.pdf';
+import resume from './kaizen_resume_sept_2024.pdf';
 
 const HeaderLink = ({ title }) => {
   const titleHref = () => {
